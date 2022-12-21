8b6ae758a49f1e8fce2e18efa97fb86c1b198383
pytsko <pytsko.i@gmail.com>
2022-12-21 22:39:09 +0300

    Create project

--- counting2/ViewController.swift
+++ counting2/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var a = 0
+    @IBOutlet weak var label: UILabel!
+    @IBAction func Count(_ sender: Any) {
+        a += 1
+        label.text = "Значение счетчика: " + String(a)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
 }
 
